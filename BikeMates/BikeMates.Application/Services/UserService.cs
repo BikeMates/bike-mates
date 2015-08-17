c96935f40d51e22128cda3e2452ccc6837b213e2
VasylKorzhyk <surfjung@gmail.com>
2015-08-17 19:33:58 +0300

    Add UserService

---
+++ BikeMates/BikeMates.Application/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using BikeMates.Contracts;
+using BikeMates.DataAccess.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeMates.Application.Services
+{
+    public class UserService:IUserService
+    {
+        private IUserRepository repository;
+
+        public UserService(IUserRepository repository)
+        {
+            this.repository = repository;
+        }
+        public ApplicationUser GetUser(string Id)
+        {
+            return this.repository.Get(Id);
+        }
+
+        public void Delete(string id)
+        {
+            var user = GetUser(id);
+            repository.Delete(user);
+        }
+    }
+}
---
+++ BikeMates/BikeMates.Contracts/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using BikeMates.DataAccess.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeMates.Contracts
+{
+    public interface IUserService
+    {
+        ApplicationUser GetUser(string Id);
+        void Delete(string Id);
+
+    }
+}
