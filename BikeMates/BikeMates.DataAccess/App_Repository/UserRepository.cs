1212524813d3b38ae692907d7786b5bb8804c1a8
VasylKorzhyk <surfjung@gmail.com>
2015-08-17 18:59:01 +0300

    UserRepository

---
+++ BikeMates/BikeMates.DataAccess/App_Repository/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BikeMates.Contracts;
+using BikeMates.DataAccess.Entity;
+
+namespace BikeMates.DataAccess.App_Repository
+{
+    class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public void Add(ApplicationUser entity)
+        {
+            //this.context.Set<ApplicationUser>().Add(entity);
+            //SaveChanges();
+        }
+
+        public void Delete(ApplicationUser entity)
+        {
+            this.context.Set<ApplicationUser>().Remove(entity);
+            SaveChanges();
+        }
+
+        public IEnumerable<ApplicationUser> GetAll()
+        {
+            return this.context.Users.ToList();
+        }
+
+        public ApplicationUser Get(string id)
+        {
+           return this.context.Set<ApplicationUser>().Find(id);
+        }
+
+        public void SaveChanges()
+        {
+            this.context.SaveChanges();
+        }
+    }
+}
---
+++ BikeMates/BikeMates.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using BikeMates.DataAccess.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeMates.DataAccess
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Route> Routes { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
