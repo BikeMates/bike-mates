3a1782726d4cd3aaa50d0a8c79f63c53c13778cf
VasylKorzhyk <surfjung@gmail.com>
2015-08-18 17:01:29 +0300

    Add register ui, login ui

--- BikeMates/BikeMates.Web/Models/AccountViewModels.cs
+++ BikeMates/BikeMates.Web/Models/AccountViewModels.cs
@@ -66,6 +66,11 @@ namespace BikeMates.Web.Models
     {
         [Required]
         [EmailAddress]
+        [Display(Name = "Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
@@ -76,7 +81,7 @@ namespace BikeMates.Web.Models
         public string Password { get; set; }
 
         //[DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Re-type password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
