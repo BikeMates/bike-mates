be498f906fcec7046aa2089681ea23e87cb77829
Messering <messering@yandex.ru>
2015-08-19 18:02:33 +0300

    HomeRouteListRepository add

--- BikeMates/BikeMates.Web/Controllers/HomeController.cs
+++ BikeMates/BikeMates.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BikeMates.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,13 @@ namespace BikeMates.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly BikeMatesDbContext context;
         public ActionResult Index()
         {
-            return View();
+            HomeRouteListRepository routelist = new HomeRouteListRepository();
+            var allRoutes = routelist.GetAllRoutes().ToList();
+
+            return View(allRoutes);
         }
 
         public ActionResult About()
