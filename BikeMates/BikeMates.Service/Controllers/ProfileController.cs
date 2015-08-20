5da41f021cfc4c81eb12fab22056bb8b930e72a5
mattbogdan <matthewbgdn@gmail.com>
2015-08-20 14:27:33 +0300

    Create Profile View - added KnockoutJs , added js files for pages

--- BikeMates/BikeMates.Service/Controllers/ProfileController.cs
+++ BikeMates/BikeMates.Service/Controllers/ProfileController.cs
@@ -12,9 +12,6 @@ using BikeMates.Contracts.Services;
 using BikeMates.DataAccess.Repository;
 using BikeMates.DataAccess;
 
-// why not working ?
-//The type 'Microsoft.AspNet.Identity.EntityFramework.IdentityUser' is defined in an assembly that is not referenced. You must add a reference to assembly 'Microsoft.AspNet.Identity.EntityFramework
-
 namespace BikeMates.Service.Controllers
 {
     public class ProfileController : ApiController
@@ -22,14 +19,13 @@ namespace BikeMates.Service.Controllers
         private IUserService userService = new UserService(new UserRepository(new BikeMatesDbContext()));
 
         public User Get(string id)
-       {
-
+        {
            return userService.GetUser(id);
         }
 
         public void Update(User user)
         {
-            userService.Update(user);
+           userService.Update(user);
         }
 
 
