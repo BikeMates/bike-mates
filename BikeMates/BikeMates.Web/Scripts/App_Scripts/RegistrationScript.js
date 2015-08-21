882d3889edb698d4b28f89fa3348c01d0ae2492c
VasylKorzhyk <surfjung@gmail.com>
2015-08-21 11:13:06 +0300

    Create BLL layer for Registration - Add RegisterScript and LoginScript

--- BikeMates/BikeMates.Web/Scripts/App_Scripts/RegistrationScript.js
+++ BikeMates/BikeMates.Web/Scripts/App_Scripts/RegistrationScript.js
@@ -1,1 +1,16 @@
-﻿
+﻿$(document).ready(function () {
+    $("#Register").click(function () {
+        $.ajax({
+            url: 'http://localhost:51952/api/account/register',
+            type: 'POST',
+            contentType: 'application/x-www-form-urlencoded',
+            data: { Email: $('#EmailReg').val(), Password: $('#PasswordReg').val(), ConfirmPassword: $('#ConfirmPassword').val(), FirstName: $('#FirstName').val() },
+            success: function (data) {
+                alert('Registration successful');
+            },
+            error: function (data) {
+                alert('Registration failed');
+            }
+        });
+    });
+});
