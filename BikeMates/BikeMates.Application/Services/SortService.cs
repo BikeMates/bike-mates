d32dd4b5a3acd34ca875f715550a6625857afdb9
Messering <messering@yandex.ru>
2015-08-21 17:30:56 +0300

    Home navigation "add bll SortRoute"

---
+++ BikeMates/BikeMates.Application/Services/SortService.cs
@@ -0,0 +1,29 @@
+﻿using BikeMates.Contracts.Services;
+using BikeMates.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeMates.Application.Services
+{
+    public class SortService : ISortService
+    {
+
+        public List<Route> Date(List<Route> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Route> NameRoute(List<Route> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Route> Participants(List<Route> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BikeMates/BikeMates.Contracts/Services/ISortService.cs
@@ -0,0 +1,16 @@
+﻿using BikeMates.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeMates.Contracts.Services
+{
+ public interface ISortService
+    {
+        List<Route> Date(List<Route> entity);
+        List<Route> NameRoute(List<Route> entity);
+        List<Route> Participants(List<Route> entity);
+    }
+}
