4c8779b9318f4c6f3f9b8b6820d98cbb493fcec3
VasylKorzhyk <surfjung@gmail.com>
2015-08-21 18:53:33 +0300

    Create Registration form - add knockout.js to Sign in/ Sign up forms

--- BikeMates/BikeMates.Web/Scripts/App_Scripts/LoginScript.js
+++ BikeMates/BikeMates.Web/Scripts/App_Scripts/LoginScript.js
@@ -1,16 +1,25 @@
 ﻿$(document).ready(function () {
-    $("#Login").click(function () {
-        $.ajax({
-            url: 'http://localhost:51952/token',
-            type: 'POST',
-            contentType: 'application/x-www-form-urlencoded',
-            data: { grant_type: 'password', username: $('#Email').val(), Password: $('#Password').val() },
-            success: function (data) {
-                alert('Your token: ' + data.access_token);
-            },
-            error: function (data) {
-                alert("Error! Data:" + data);
-            }
-        });
-    });
-});
+
+    function LoginViewModel() {
+        this.username = ko.observable("");
+        this.password = ko.observable("");
+        this.login = function()
+        {
+            $.ajax({
+                url: 'http://localhost:51952/token',
+                type: 'POST',
+                contentType: 'application/x-www-form-urlencoded',
+                data: { grant_type: 'password', username: this.username, Password: this.password },
+                success: function (data) {
+                    alert('Your token: ' + data.access_token);
+                },
+                error: function (data) {
+                    alert("Error! Data:" + data);
+                }
+            });
+        }
+    }
+
+    ko.applyBindings(new LoginViewModel());
+  });
+
--- BikeMates/BikeMates.Web/Scripts/App_Scripts/RegistrationScript.js
+++ BikeMates/BikeMates.Web/Scripts/App_Scripts/RegistrationScript.js
@@ -1,16 +1,27 @@
 ﻿$(document).ready(function () {
-    $("#Register").click(function () {
-        $.ajax({
-            url: 'http://localhost:51952/api/account/register',
-            type: 'POST',
-            contentType: 'application/x-www-form-urlencoded',
-            data: { Email: $('#EmailReg').val(), Password: $('#PasswordReg').val(), ConfirmPassword: $('#ConfirmPassword').val(), FirstName: $('#FirstName').val() },
-            success: function (data) {
-                alert('Registration successful');
-            },
-            error: function (data) {
-                alert('Registration failed');
-            }
-        });
-    });
+
+    function RegisterViewModel() {
+
+        this.firstName = ko.observable("");
+        this.secondName = ko.observable("");
+        this.email = ko.observable("");
+        this.password = ko.observable("");
+
+        this.register = function () {
+            $.ajax({
+                url: 'http://localhost:51952/api/account/register',
+                type: 'POST',
+                contentType: 'application/x-www-form-urlencoded',
+                data: { Email: this.email, Password: this.password, ConfirmPassword: this.password, FirstName: this.firstName, SecondName: this.secondName },
+                success: function (data) {
+                    alert('Registration successful');
+                },
+                error: function (data) {
+                    alert('Registration failed');
+                }
+            });
+        }
+    }
+
+    ko.applyBindings(new RegisterViewModel());
 });
