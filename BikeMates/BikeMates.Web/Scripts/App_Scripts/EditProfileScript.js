551e5bbdf2c6d3a935249fc5a07a5b9dc55eb11b
mattbogdan <matthewbgdn@gmail.com>
2015-08-25 17:00:11 +0300

    Create BLL for profile -added image upload

--- BikeMates/BikeMates.Web/Scripts/App_Scripts/EditProfileScript.js
+++ BikeMates/BikeMates.Web/Scripts/App_Scripts/EditProfileScript.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
 
     function AppViewModel() {
-
+        uploads: ko.observableArray([]);
         var self = this;
         self.FirstName = ko.observable("");
         self.SecondName = ko.observable("");
@@ -56,7 +56,75 @@ $(document).ready(function () {
 
         });
 
+
+
+
+   $("#upload_btn").button().click(function () {
+        $.ajax({
+            url: 'http://localhost:51952/api/profilepicture',
+            type: 'POST',
+            data: new FormData($('#files')[0]), // The form with the file inputs.
+            processData: false                          // Using FormData, no need to process data.
+        }).done(function () {
+            alert("Success: Files sent!");
+        }).fail(function () {
+            alert("An error occurred, the files couldn't be sent!");
+        });
+    });
+
+
+
+        var input = document.getElementById("file");
+        var formdata = false;
+        if (window.FormData) {
+            formdata = new FormData();
+        }
+        input.addEventListener("change", function (evt) {
+            var i = 0, len = this.files.length, img, reader, file;
+
+            for (; i < len; i++) {
+                file = this.files[i];
+
+                if (!!file.type.match(/image.*/)) {
+                    if (window.FileReader) {
+                        reader = new FileReader();
+                        reader.onloadend = function (e) {
+                            //showUploadedItem(e.target.result, file.fileName);
+                        };
+                        reader.readAsDataURL(file);
+                    }
+
+                    if (formdata) {
+                        formdata.append("image", file);
+                        formdata.append("extra", 'extra-data');
+                    }
+
+                    if (formdata) {
+                        jQuery('div#response').html('<br /><img src="ajax-loader.gif"/>');
+
+                        jQuery.ajax({
+                            url: "http://localhost:51952/api/profilepicture",
+                            type: "POST",
+                            data: formdata,
+                            processData: false,
+                            contentType: false,
+                            success: function (res) {
+                                jQuery('div#response').html("Successfully uploaded");
+                            }
+                        });
+                    }
+                }
+                else {
+                    alert('Not a vaild image!');
+                }
+            }
+
+        }, false);
+
+
     }
+
+
 
     // Activates knockout.js
 
@@ -64,6 +132,8 @@ $(document).ready(function () {
     ko.applyBindings(new AppViewModel());
 
 });
+
+
 
 
 
