4889521b0489970ed1f239b28f5cabdd851fd138
VasylKorzhyk <surfjung@gmail.com>
2015-08-26 17:47:56 +0300

    Add forgot password page - added Captcha

---
+++ BikeMates/BikeMates.Web/Models/CaptchaModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class CaptchaModel
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        [JsonProperty("error-codes")]
+        public List<string> ErrorCodes { get; set; }
+    }
+}
