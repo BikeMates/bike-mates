d6e8fbe22ac6f00a46b1b68b2a14518ad4d68401
Messering <messering@yandex.ru>
2015-08-27 18:38:45 +0300

    Route view- add demo route view

---
+++ BikeMates/BikeMates.Web/Scripts/App_Scripts/RouteView.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+
+    function AppViewModel() {
+
+        var self = this;
+        self.title = ko.observable("");
+        self.start = ko.observable("");
+        self.distance = ko.observable("");
+        self.meeting_place = ko.observable("");
+
+
+        $.ajax({
+            url: "http://localhost:51952/api/route",
+            contentType: "application/json",
+            type: "GET",
+            success: function (data) {
+
+                self.title(data.title);
+                self.start(data.start);
+                self.distance(data.distance);
+                self.meeting_place(data.meeting_place);
+
+            },
+            error: function (data) {
+
+            }
+        });
+    }
+    ko.applyBindings(new AppViewModel());
+
+});
+
