baaad59aeebd0d3f7337bcbe38660485d2fb1131
VasylKorzhyk <surfjung@gmail.com>
2015-08-27 18:37:26 +0300

    Create site navigation - added login form

--- BikeMates/BikeMates.Web/App/pages/login/login.js
+++ BikeMates/BikeMates.Web/App/pages/login/login.js
@@ -1,8 +1,36 @@
 ﻿define(["knockout", "text!./login.html"], function (ko, loginTemplate) {
 
+    var tokenKey = "tokenInfo";
+
     function LoginViewModel(params) {
         var self = this;
+        self.username = ko.observable("");
+        self.password = ko.observable("");
+        self.login = function () {
+            $.ajax({
+                url: 'http://localhost:51952/api/account/login',
+                type: 'POST',
+                contentType: 'application/x-www-form-urlencoded',
+                data:
+                    {
+                        grant_type: 'password',
+                        email: this.username,
+                        Password: this.password
+                    },
+                success: function (data) {
+                    $("#error_message").hide();
 
+                    sessionStorage.setItem(tokenKey, data.access_token);
+                    window.location.href = "/index.html";
+                },
+                error: function (data) {
+                    var response = JSON.parse(data.responseText);
+
+                    $("#error_details").text(response.error_description)
+                    $("#error_message").show();
+                }
+            });
+        }
         return self;
     }
     return { viewModel: LoginViewModel, template: loginTemplate };
