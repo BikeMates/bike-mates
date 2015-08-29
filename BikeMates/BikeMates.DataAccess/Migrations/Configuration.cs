5ab177bc08e50069affde6c5fbd41d5b2bc79058
VasylKorzhyk <surfjung@gmail.com>
2015-08-29 19:38:19 +0300

    Add role handling to UI - created roles

--- BikeMates/BikeMates.DataAccess/Migrations/Configuration.cs
+++ BikeMates/BikeMates.DataAccess/Migrations/Configuration.cs
@@ -1,5 +1,9 @@
 namespace BikeMates.DataAccess.Migrations
 {
+    using BikeMates.DataAccess.Managers;
+    using BikeMates.Domain.Entities;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -26,6 +30,26 @@ namespace BikeMates.DataAccess.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            var userManager = new UserManager(context);
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            var role1 = new IdentityRole { Name = "admin" };
+            var role2 = new IdentityRole { Name = "user" };
+
+            roleManager.Create(role1);
+            roleManager.Create(role2);
+
+            var admin = new User { Email = "admin@admin.com", UserName = "admin@admin.com", Role = "admin" };
+            string password = "Qwerty1#";
+            var result = userManager.Create(admin, password);
+
+            if (result.Succeeded)
+            {
+                userManager.AddToRole(admin.Id, role1.Name);
+            }
+ 
         }
     }
 }
