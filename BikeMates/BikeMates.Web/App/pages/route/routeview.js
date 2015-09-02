55603cfbeb03b71b6535c7c2f46d71d0c0313114
Messering <messering@yandex.ru>
2015-09-02 17:38:02 +0300

    Ui RouteView- move to app/page

---
+++ BikeMates/BikeMates.Web/App/pages/route/routeview.js
@@ -0,0 +1,36 @@
+﻿define(["knockout", "text!./routeview.html", "require", "cssLoader"], function (ko, routeviewTemplate, require, cssLoader) {
+
+    var localPath = "/Content/Site.css";
+    var pathFromApp = require.toUrl(localPath);
+
+    cssLoader.link(pathFromApp);
+    function RoutevViewModel() {
+
+        var self = this;
+        self.title = ko.observable("");
+        self.start = ko.observable("");
+        self.distance = ko.observable("");
+        self.MeetingPlace = ko.observable("");
+        self.Participants = ko.observable();
+        //self.Start = ko.observable();
+        //self.End = ko.observable();
+        self.ByTitle = ko.observable("");
+        $.ajax({
+            url: "http://localhost:51952/api/route/viewmodel",
+            contentType: "application/json",
+            type: "GET",
+            success: function (data) {
+                //self.Start(data.Start)
+                //self.End(data.End)
+                self.title(data.title);
+                self.start(data.start);
+                self.distance(data.distance);
+                self.MeetingPlace(data.meetingPlace);
+            },
+            error: function (data) {
+            }
+        });
+    }
+    return { viewModel: RoutevViewModel, template: routeviewTemplate };
+});
+
