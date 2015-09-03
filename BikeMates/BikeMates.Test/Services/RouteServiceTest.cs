5b383916ced7a65d14129e6fe34d2547815818b9
BohdanMatviiv <matthewbgdn@gmail.com>
2015-09-03 23:56:21 +0300

    Cover RouteService with unit tests - edited test

--- BikeMates/BikeMates.Test/Services/RouteServiceTest.cs
+++ BikeMates/BikeMates.Test/Services/RouteServiceTest.cs
@@ -1,36 +1,66 @@
 ﻿using BikeMates.Application.Services;
 using BikeMates.Contracts.Repositories;
-using BikeMates.DataAccess.Managers;
-using BikeMates.DataAccess.Repository;
+using BikeMates.Contracts.Services;
 using BikeMates.Domain.Entities;
-using BikeMates.Domain.Entities;
-using BikeMates.DataAccess.Managers;
+using Moq;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BikeMates.Test
 {
     [TestFixture]
     class RouteServiceTests
     {
+        private Mock<IUserRepository> userRepository;
+        private IUserService userService;
+        private Mock<IRouteRepository> routeRepository;
+        private IRouteService routeService;
+
         [TestFixtureSetUp]
         public void InitialSetup()
         {
-
-
+            userRepository = new Mock<IUserRepository>();
+            userService = new UserService(userRepository.Object);
+            routeRepository = new Mock<IRouteRepository>();
+            routeService = new RouteService(routeRepository.Object, userRepository.Object);
         }
 
         [Test]
-        public void AddRouteTest()
+        [TestCase("test@test.com", "Dima", "Plahta", "Like cycling", "Route description", 2.44, "Shevchenka blv", 1, TestName = "AddedInfoShouldBeIdentical")]
+        public void AddRouteTest(string expectedEmail, string expectedName, string expectedSurname, string expectedDescription, string expectedAbout
+            , double expectedDistance, string expectedPlace, int expectedId)
         {
-            Route r1 = new Route() {   Id = 1 , Title = "loool"};
-            Route r2 = new Route() {   Id = 2 , Title = "loool"};
-            
-            Assert.AreEqual(r1.Title , r2.Title);
+            var expectedUser = new User
+            {
+                Email = expectedEmail,
+                About = expectedAbout,
+                FirstName = expectedName,
+                SecondName= expectedSurname
+            };
+
+            var expectedRoute = new Route 
+            {
+               Id = expectedId,
+               Author = expectedUser,
+               Description = expectedDescription,
+               Start = new DateTime(2015,12,22),
+               Distance = expectedDistance,
+               MeetingPlace = expectedPlace 
+            };
+
+          
+          routeRepository.Setup(r => r.Find(It.IsAny<int>())).Returns(expectedRoute);
+
+            Route testroute = routeService.Find(1);
+
+            Assert.AreEqual(expectedEmail ,      testroute.Author.Email ,     " Emails are not equal" );
+            Assert.AreEqual(expectedName,        testroute.Author.FirstName,  " First Names are not equal");
+            Assert.AreEqual(expectedSurname,     testroute.Author.SecondName, " Second Names are not equal");
+            Assert.AreEqual(expectedDescription, testroute.Description,       " Descriptions are not equal");
+            Assert.AreEqual(expectedAbout,       testroute.Author.About,      " Abouts are not equal");
+            Assert.AreEqual(expectedDistance,    testroute.Distance,          " Distances are not equal");
+            Assert.AreEqual(expectedPlace,       testroute.MeetingPlace,      " Places are not equal");
+            Assert.AreEqual(expectedId,          testroute.Id,                " Ids are not equal");
         }
 
 
