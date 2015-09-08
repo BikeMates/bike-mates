4909b6a258a6f33a5f40a1a642b365d92a4ecb66
VasylKorzhyk <surfjung@gmail.com>
2015-09-08 11:44:56 +0300

    Fixed "AllRoutes" and "Add new rotue" hide after sign out

--- BikeMates/BikeMates.Web/Scripts/App_Scripts/LayoutScript.js
+++ BikeMates/BikeMates.Web/Scripts/App_Scripts/LayoutScript.js
@@ -4,5 +4,6 @@
         $('#userpanel').find('#anonim').show();
         $('#userpanel').find('#user').hide();
         $('#userpanel').find('#admin').hide();
+        $('#page1').find('#userFunctions').hide();
     });
 });
